caf416beca7e193ebd72205fca7992bdf86ae9ad
Sigmadesire <hruthik.me22@bmsce.ac.in>
2026-05-07 14:44:32 +0530

    Day 1 Setup Flask folder structure and health endpoint

---
+++ ai-service/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/health", methods=["GET"])
+def health():
+    return jsonify({
+        "status": "UP",
+        "service": "ai-service"
+    }), 200
+
+@app.route("/", methods=["GET"])
+def home():
+    return jsonify({
+        "message": "AI Service is running"
+    }), 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
